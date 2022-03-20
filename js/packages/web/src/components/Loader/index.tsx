cef9598a19df73a242326abf5ba53266b147075b
SOLProprietor <jrlevy@gmail.com>
2022-03-19 21:30:46 -0700

    pretty updates

--- js/packages/web/src/components/Loader/index.tsx
+++ js/packages/web/src/components/Loader/index.tsx
@@ -19,16 +19,63 @@ export const LoaderProvider: FC = ({ children }) => {
 
 export const Spinner = () => {
   return (
-    <div className="spinner">
-      <span className="line line-1" />
-      <span className="line line-2" />
-      <span className="line line-3" />
-      <span className="line line-4" />
-      <span className="line line-5" />
-      <span className="line line-6" />
-      <span className="line line-7" />
-      <span className="line line-8" />
-      <span className="line line-9" />
+    //<div className="spinner">
+    //  <span className="line line-1" />
+    //  <span className="line line-2" />
+    // <span className="line line-3" />
+    //  <span className="line line-4" />
+    //  <span className="line line-5" />
+    // <span className="line line-6" />
+    //  <span className="line line-7" />
+    //  <span className="line line-8" />
+    // <span className="line line-9" />
+
+    <div className="duckloader">
+      <div className="helper--1">
+        <div className="duck-wrapper">
+          <div className="duck-hide"></div>
+          <div className="duck-body"></div>
+          <div></div>
+          <div className="duck-head"></div>
+          <div className="duck-eye"></div>
+        </div>
+        <div className="duck-mouth"></div>
+      </div>
+
+      <div className="helper--2">
+        <div className="duck-wrapper">
+          <div className="duck-hide"></div>
+          <div className="duck-body"></div>
+          <div></div>
+          <div className="duck-head"></div>
+          <div className="duck-eye"></div>
+        </div>
+        <div className="duck-mouth"></div>
+      </div>
+
+      <div className="helper--3">
+        <div className="duck-wrapper">
+          <div className="duck-hide"></div>
+          <div className="duck-body"></div>
+          <div></div>
+          <div className="duck-head"></div>
+          <div className="duck-eye"></div>
+        </div>
+        <div className="duck-mouth"></div>
+      </div>
+
+      <div className="duck-land"></div>
+
+      <div className="bubble bubble--1"></div>
+      <div className="bubble bubble--2"></div>
+      <div className="bubble bubble--3"></div>
+      <div className="bubble bubble--4"></div>
+      <div className="bubble bubble--5"></div>
+      <div className="bubble bubble--6"></div>
+      <div className="bubble bubble--7"></div>
+      <div className="bubble bubble--8"></div>
     </div>
+
+    //</div>
   );
 };
